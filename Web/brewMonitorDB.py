22692a3b2f19fdc55093500550aa189d3b1e8220
catchmartin <catchmartin@gmail.com>
2015-01-19 20:21:52 +0000

    Tidied up python script a bit for better management of DB

--- Web/brewMonitorDB.py
+++ Web/brewMonitorDB.py
@@ -1,7 +1,5 @@
 import MySQLdb
 import gc
-import smtplib
-from email.mime.text import MIMEText
 import time
 from datetime import datetime, timedelta
 
@@ -9,20 +7,8 @@ averageTemp="0"
 stdTemp="0"
 averageHeatOn="-1"
 
-def send():
-        msg = MIMEText("This is an automated message to say that we've hit the 20c mark. Now hopefully the controller should keep it around that temperature level.")
-        msg['Subject'] = 'Target temperature reached!'
-        msg['From'] = 'from@mail.com'
-        msg['To'] = 'to@mail.com'
-        s = smtplib.SMTP('localhost')
-        s.sendmail(msg['From'], msg['To'], msg.as_string())
-        s.quit()
-	global x
-	x=False
-
 gc.collect()
-db = MySQLdb.connect(host="localhost", port=3306, user="username", 
-passwd="password", db="brewmonitor")
+db = MySQLdb.connect(host="localhost", port=3306, user="USERNAME", passwd="PASSWORD", db="brewmonitor")
 cursor = db.cursor()
 now = datetime.today() - timedelta(hours = 1)
 updateString = datetime.today().strftime('%Y-%m-%d %H')+":00:00"
@@ -35,31 +21,35 @@ hourAgoString = lastHourDateTime.strftime('%Y-%m-%d %H')+":00:01"
 statement="SELECT AVG(temp) FROM readings WHERE device=2 AND created_at between '"+hourAgoString+"' AND '"+nowString+"'"
 #print statement
 cursor.execute(statement)
-for row in cursor.fetchall() :
+row = cursor.fetchone()
+if row[0] != None:
 	averageTemp = "{0:.2f}".format(row[0])
 
 statement="SELECT STD(temp) FROM readings WHERE device=2 AND created_at between '"+hourAgoString+"' AND '"+nowString+"'"
 #print statement
 cursor.execute(statement)
-for row in cursor.fetchall() :
+row = cursor.fetchone()
+if row[0] != None:
 	stdTemp = "{0:.2f}".format(row[0])
 
 statement="SELECT AVG(heaton) FROM readings WHERE device=2 AND created_at between '"+hourAgoString+"' AND '"+nowString+"'"
 #print statement
 cursor.execute(statement)
-for row in cursor.fetchall() :
-	if row[0] != None:
-               	averageHeatOn = "{0:.2f}".format(row[0])
+row = cursor.fetchone()
+if row[0] != None:
+	averageHeatOn = "{0:.2f}".format(row[0])
 
-statement="DELETE FROM readings WHERE device=2 AND created_at between '"+hourAgoString+"' AND '"+nowString+"'"
-#print statement
-cursor.execute(statement)
-db.commit()
+try:
+	statement="DELETE FROM readings WHERE device=2 AND created_at between '"+hourAgoString+"' AND '"+nowString+"'"
+	#print statement
+	cursor.execute(statement)
 
-statement="INSERT INTO readings(device, temp, stdtemp, heaton, created_at, updated_at) VALUES (2,"+averageTemp+","+stdTemp+","+averageHeatOn+",'"+nowString+"','"+updateString+"')"
-#print statement
-cursor.execute(statement)
-db.commit()
+	statement="INSERT INTO readings(device, temp, stdtemp, heaton, created_at, updated_at) VALUES (2,"+averageTemp+","+stdTemp+","+averageHeatOn+",'"+nowString+"','"+updateString+"')"
+	#print statement
+	cursor.execute(statement)
+	db.commit()
+except:
+	db.rollback()
 
 #print updateString
 #print nowString
